e4a3b96496eeeacc87c0945637e57664657c2a71
Parker <38483827+alanoakes@users.noreply.github.com>
2021-12-14 20:22:33 -0600

    Use youtube-dl and its options

---
+++ Youtube-dwnlds.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#source: https://linuxconcept.com/how-to-install-youtube-dl-on-ubuntu-20-04-linux-operating-system/
+
+# Audio Format
+#youtube-dl https://www.youtube.com/watch?v=WadvtSAKoAY -x --audio-format mp3
+
+# Mp4
+#youtube-dl -f 18 https://www.youtube.com/watch?v=HmKPFD_3PoQ
+
+# Youtube-dl has the capabilities to support more video formats such as MP4,
+# 3GP, WebM, and FLV. You can check all the available video format for any
+# video by using below command:
+
+#youtube-dl -F https://www.youtube.com/watch?v=HmKPFD_3PoQ
+
+
+#Bible Project: Visual Commentaries
+#youtube-dl https://www.youtube.com/watch?v=k8P-x34iYRE -x --audio-format mp3
+#youtube-dl https://www.youtube.com/watch?v=GOXEADdM0ZI -x --audio-format mp3
+#youtube-dl https://www.youtube.com/watch?v=LBr-blQxIm4 -x --audio-format mp3
+#youtube-dl https://www.youtube.com/watch?v=JCP2zWaJlGc -x --audio-format mp3
+#youtube-dl https://www.youtube.com/watch?v=rhc1SjvYXqE -x --audio-format mp3
+#youtube-dl https://www.youtube.com/watch?v=NluVlZCToSg -x --audio-format mp3
+
+# PragerU-Kids
+youtube-dl -f 18 https://www.youtube.com/watch?v=hNm10fK2y7U
+youtube-dl -f 18 https://www.youtube.com/watch?v=nn9G7S_yn40
+youtube-dl -f 18 https://www.youtube.com/watch?v=B3aMzgwpYC0
+youtube-dl -f 18 https://www.youtube.com/watch?v=Ym3o_LIOMDE
+youtube-dl -f 18 https://www.youtube.com/watch?v=gLXBMLSdh3E
+youtube-dl -f 18 https://www.youtube.com/watch?v=SSzZHjjw4DI
+youtube-dl -f 18 https://www.youtube.com/watch?v=nRdRvhkCBvY
+youtube-dl -f 18 https://www.youtube.com/watch?v=qvOdkuS1jGM
+youtube-dl -f 18 https://www.youtube.com/watch?v=p6EXj6CblpA
+youtube-dl -f 18 https://www.youtube.com/watch?v=_TUVfQ-QZ7A
+youtube-dl -f 18 https://www.youtube.com/watch?v=MckBflKXTv0
+youtube-dl -f 18 https://www.youtube.com/watch?v=a7x4LMTbnYo
+youtube-dl -f 18 https://www.youtube.com/watch?v=FoRlz1DWK2U
+youtube-dl -f 18 https://www.youtube.com/watch?v=fDfd8_iq0tc
+youtube-dl -f 18 https://www.youtube.com/watch?v=d3dkGJHWBQY
+youtube-dl -f 18 https://www.youtube.com/watch?v=p-CDM9MhdRA
+youtube-dl -f 18 https://www.youtube.com/watch?v=Y6OYsa_x534
