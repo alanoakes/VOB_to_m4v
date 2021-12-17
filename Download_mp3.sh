e30f0877e3978e29cdcff1c20a9dc9da62fdb4c9
Parker <38483827+alanoakes@users.noreply.github.com>
2021-12-17 10:01:49 -0600

    Initial commit
    This file is to take either a video or audio file and make it into MP3 (ie podcast version)

---
+++ Download_mp3.sh
@@ -0,0 +1,4 @@
+# youtube-dl -x --audio-format mp3 video_URL
+# fox news video from FreshRSS
+  # go to link > press share to email > grab link from email
+youtube-dl -x --audio-format mp3 https://video.foxnews.com/v/6287232314001
